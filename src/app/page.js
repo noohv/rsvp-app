b15751a2d7410c9ecaabebca76da3f8e864febe5
noohv <valters.huuns@gmail.com>
2023-12-06 01:32:45 +0200

    survey

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,84 @@
 "use client"
 import React, { useState, useEffect } from 'react'
+import 'survey-core/defaultV2.min.css';
+import { Model } from 'survey-core'
+import { Survey } from 'survey-react-ui';
 import Test from './singleTest'
+// import Survey from './survey'
+const surveyJson = {
+  elements: [{
+    name: "sex",
+    title: "Jūsu dzimums:",
+    type: "radiogroup",
+    choices: ["Vīrietis", "Sieviete", "Cits"]
+  }, 
+  {
+    name: "age",
+    title: "Jūsu vecums:",
+    type: "text"
+  },
+  {
+    type: "checkbox",
+    name: "promoter-features",
+    title: {
+      default: "Which of the following features do you value the most?",
+    },
+    description: {
+      default: "Please select no more than three features.",
+    },
+    isRequired: true,
+    choices: [
+      {
+        value: "performance",
+        text: "Performance"
+      },
+      {
+        value: "stability",
+        text: {
+          default: "Stability",
+        }
+      },
+      {
+        value: "ui",
+        text: {
+          default: "User interface",
+        }
+      },
+      {
+        value: "complete-functionality",
+        text: {
+          default: "Complete functionality",
+        }
+      },
+      {
+        value: "learning-materials",
+        text: {
+          default: "Learning materials (documentation, demos, code examples)",
+        }
+      },
+      {
+        value: "support",
+        text: {
+          default: "Quality support",
+        }
+      }
+    ],
+    showOtherItem: true,
+    otherPlaceholder: {
+      default: "Please specify...",
+    },
+    otherText: {
+      default: "Other features",
+    },
+    colCount: 2,
+    maxSelectedChoices: 3
+  }
+
+]
+};
 
 function Home() {
+  const survey = new Model(surveyJson);
   const [phase, setPhase] = useState("user")
   const [reactionTime, setReactionTime] = useState(null)
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -62,16 +138,21 @@ function Home() {
 		<>
 
       {phase === "user" &&
+        <>
+          <Survey model={survey} />
           <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-50" onClick={(e) => {setPhase("test")}}>
             Uz testu
           </button>
-
+        </>
       }
+
+      {currentTest == 2 ? !isActive ? "Yes" : "No" : ""}
 
 
       {(phase === "test" && currentTest == 1) &&
         <Test testNumber={1} setReactionTime={setReactionTime} setIsActive={setIsActive}/>
       }
+
 
       {(phase === "test" && currentTest == 2) &&
         <Test testNumber={2} setReactionTime={setReactionTime} setIsActive={setIsActive}/>
---
+++ src/app/survey.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Survey = () => {
+
+      
+
+  return (
+    <div>Survejs</div>
+  )
+}
+
+export default Survey
