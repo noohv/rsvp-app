ec3d3f9f2cfd3f7b86b91f283e498c63f2f5b31b
noohv <valters.huuns@gmail.com>
2023-12-06 22:18:55 +0200

    Test

--- src/app/tuto.js
+++ src/app/tuto.js
@@ -6,15 +6,15 @@ const Tutorial = ({currentIndex}) => {
     <div className='absolute top-50 right-50 w-2/4'>
         {currentIndex == 0 && 
             <p className='text-justify'>
-                Tālāk būs 3 testi. Katra testā sākumā aptuveni sekundi tiks rādīts "+" simbols, pēc tā ļoti ātri sāks mainīties burti.
-                Brīdī, kad redzi burtu "T" pēc iespējas ātrāk spied pogu.
-                Ja viss ir skaidrs, tad spied uz "Sākt testu"
+                Tālāk būs 3 testi. Katra testā sākumā aptuveni sekundi tiks rādīts + simbols, pēc tā ļoti ātri sāks mainīties burti.
+                Brīdī, kad redzi burtu T pēc iespējas ātrāk spied pogu.
+                Ja viss ir skaidrs, tad spied uz Sākt testu
             </p>
         }
 
         {(currentIndex == 1 || currentIndex == 2) && 
             <p className='text-justify'>
-                Spied "Sākt testu", lai sāktu nākamo testu
+                Spied Sākt testu, lai sāktu nākamo testu
             </p>
         }
     </div>
