364a2cca1d67814324f8f61780ae99361c65c6ec
noohv <valters.huuns@gmail.com>
2023-12-14 01:34:02 +0200

    Better tuto

--- src/app/tuto.js
+++ src/app/tuto.js
@@ -5,9 +5,7 @@ const Tutorial = ({currentIndex}) => {
     <div className='absolute top-50 right-50 w-2/4'>
         {currentIndex == 0 && 
             <p className='text-justify'>
-                Tālāk būs 3 testi. Katra testā sākumā aptuveni divas sekundes tiks rādīts <b>&lsquo;+&rsquo;</b> simbols, pēc tā ļoti ātri sāks mainīties burti.
-                Brīdī, kad redzi burtu <b>&lsquo;T&rsquo;</b> pēc iespējas ātrāk nospied uz pogas <b>&quot;Spied šeit&quot;</b>.
-                Ja viss ir skaidrs, tad spied uz <b>&quot;Sākt testu&quot;</b>.
+                Tālāk būs 3 testi. Testa sākumā divas sekundes tiks parādīts <b>&lsquo;+&rsquo;</b> simbols. Tad automātiski, ļoti ātrā tempā sāks mainīties burti. Mērķis ir pēc iespējas ātrāk nospiest pogu <b>&quot;Spied šeit&quot;</b> brīdī, kad redzi burtu <b>&lsquo;T&rsquo;</b>. Nospiežot pogu tests neapstāsies, tādēļ nespied to vairākas reizes, ja burts <b>&lsquo;T&rsquo;</b> vairāk neparādās. Kad visi burti būs parādīti, tests automātiski beigsies. Ja viss ir nosacījumi ir skaidri un esi gatavs/-a, tad spied uz <b>&quot;Sākt testu&quot;</b>.
             </p>
         }
 
