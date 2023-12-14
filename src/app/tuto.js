1bbb6d0fda66bcdb669c3de5df6e3d2a261a1ca2
noohv <valters.huuns@gmail.com>
2023-12-14 02:08:04 +0200

    Tuto fix

--- src/app/tuto.js
+++ src/app/tuto.js
@@ -2,10 +2,12 @@ import React, { useState, useEffect } from 'react'
 
 const Tutorial = ({currentIndex}) => {
   return (
-    <div className='absolute top-50 right-50 w-2/4'>
+    <div className='absolute top-[10%] lg:top-[30%] p-5 lg:w-3/5'>
         {currentIndex == 0 && 
             <p className='text-justify'>
-                Tālāk būs 3 testi. Testa sākumā divas sekundes tiks parādīts <b>&lsquo;+&rsquo;</b> simbols. Tad automātiski, ļoti ātrā tempā sāks mainīties burti. Mērķis ir pēc iespējas ātrāk nospiest pogu <b>&quot;Spied šeit&quot;</b> brīdī, kad redzi burtu <b>&lsquo;T&rsquo;</b>. Nospiežot pogu tests neapstāsies, tādēļ nespied to vairākas reizes, ja burts <b>&lsquo;T&rsquo;</b> vairāk neparādās. Kad visi burti būs parādīti, tests automātiski beigsies. Ja viss ir nosacījumi ir skaidri un esi gatavs/-a, tad spied uz <b>&quot;Sākt testu&quot;</b>.
+                Tālāk būs 3 testi. Testa sākumā divas sekundes tiks parādīts <b>&lsquo;+&rsquo;</b> simbols. Tad automātiski, ļoti ātrā tempā sāks mainīties burti. Mērķis ir pēc iespējas ātrāk nospiest pogu <b>&quot;Spied šeit&quot;</b> brīdī, kad redzi burtu <b>&lsquo;T&rsquo;</b>. Nospiežot pogu tests neapstāsies, tādēļ nespied to vairākas reizes, ja burts <b>&lsquo;T&rsquo;</b> vairāk neparādās. Kad visi burti būs parādīti, tests automātiski beigsies. 
+                <br></br>
+                Ja visi ir nosacījumi ir skaidri un esi gatavs/-a, tad spied uz <b>&quot;Sākt testu&quot;</b>.
             </p>
         }
 
